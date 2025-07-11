b8f754bb1af3428156d88cd8c23766472e323671
uyuyuy99 <uyuyuy99@gmail.com>
2025-07-10 18:36:10 -0700

    Initial commit

---
+++ src/main/java/me/uyuyuy99/marketplace/MarketPlace.java
@@ -0,0 +1,15 @@
+package me.uyuyuy99.marketplace;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MarketPlace extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
